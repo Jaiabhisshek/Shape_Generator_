9f1c9a4e17505c36f906fb43a47b8e2abdf4cc4b
jaiabhisshek8 <jaiabhissheks@gmail.com>
2023-08-26 10:29:49 +0530

    updated

---
+++ app.js
@@ -0,0 +1,35 @@
+let $button = document.getElementById("button");
+let $number = document.getElementById("number");
+let $circle = document.getElementById("circle");
+let $square = document.getElementById("square");
+let $rectangle = document.getElementById("rectangle");
+let $box=document.getElementById("box");
+let numberInShape = 1;
+let oldValue = 0;
+
+$button.onclick=()=>{
+let noOfShapes = Number(number.value);
+noOfShapes += oldValue;
+for(let i=numberInShape; i<=noOfShapes; i++){
+    if($circle.checked==true){
+        let circle = document.createElement("div");
+        circle.innerText=i;
+        circle.setAttribute("class", "circle");
+        $box.append(circle);
+    }
+    else if($square.checked){
+        let square = document.createElement("div");
+        square.innerText=i;
+        square.setAttribute("class", "square");
+        $box.append(square);
+    }
+    else{
+        let rectangle = document.createElement("div");
+        rectangle.innerText=i;
+        rectangle.setAttribute("class", "rectangle");
+        $box.append(rectangle);
+    }
+    numberInShape++;
+}
+oldValue=noOfShapes;
+}
